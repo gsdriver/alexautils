6457d1f23351b82435eb792b3565a186cf9da6c3
Garrett Vargas <gvargas@SEA.CORP.EXPECN.COM>
2018-10-16 05:34:06 -0700

    Use multiple channels

--- threecardanalyze/main.go
+++ threecardanalyze/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-  "fmt"
-  "encoding/json"
-  "strings"
-  "strconv"
-  "time"
+    "fmt"
+    "encoding/json"
+    "strings"
+    "strconv"
+    "time"
+    "io/ioutil"
 )
 
 type WinRatio struct {
-  Wins int
-  Ties int
-  Loses int
+    Wins int
+    Ties int
+    Loses int
 }
 
 var ranking map[string]int
@@ -66,50 +67,50 @@ func oddstowin(cards []string, hold int) float64 {
   return (total / evaluated)
 }
 
-func analyzehand(cards string) ([]int, float64) {
-  hand := strings.Split(cards, "-")
-  var bestplay []int
-  bestodds := 0.0
-  var odds []float64
-  var bestMapping [][]int
+func analyzehand(ch chan []int, cards string) {
+    hand := strings.Split(cards, "-")
+    var bestplay []int
+    bestodds := 0.0
+    var odds []float64
+    var bestMapping [][]int
 
-  bestMapping = append(bestMapping, []int{0, 1, 2})
-  bestMapping = append(bestMapping, []int{0, 1})
-  bestMapping = append(bestMapping, []int{1, 2})
-  bestMapping = append(bestMapping, []int{0, 2})
-  bestMapping = append(bestMapping, []int{2})
-  bestMapping = append(bestMapping, []int{0})
-  bestMapping = append(bestMapping, []int{1})
-  bestMapping = append(bestMapping, []int{})
+    bestMapping = append(bestMapping, []int{0, 1, 2})
+    bestMapping = append(bestMapping, []int{0, 1})
+    bestMapping = append(bestMapping, []int{1, 2})
+    bestMapping = append(bestMapping, []int{0, 2})
+    bestMapping = append(bestMapping, []int{2})
+    bestMapping = append(bestMapping, []int{0})
+    bestMapping = append(bestMapping, []int{1})
+    bestMapping = append(bestMapping, []int{})
 
-  // OK, first hold all
-  odds = append(odds, oddstowin(hand, 3))
+    // OK, first hold all
+    odds = append(odds, oddstowin(hand, 3))
 
-  // Try holding two cards
-  odds = append(odds, oddstowin(hand, 2))
-  hand[0], hand[2] = hand[2], hand[0]
-  odds = append(odds, oddstowin(hand, 2))
-  hand[1], hand[2] = hand[1], hand[2]
-  odds = append(odds, oddstowin(hand, 2))
+    // Try holding two cards
+    odds = append(odds, oddstowin(hand, 2))
+    hand[0], hand[2] = hand[2], hand[0]
+    odds = append(odds, oddstowin(hand, 2))
+    hand[1], hand[2] = hand[1], hand[2]
+    odds = append(odds, oddstowin(hand, 2))
 
-  // Try holding one card (note array is reversed at this point)
-  odds = append(odds, oddstowin(hand, 1))
-  hand[0], hand[2] = hand[2], hand[0]
-  odds = append(odds, oddstowin(hand, 1))
-  hand[1], hand[0] = hand[0], hand[1]
-  odds = append(odds, oddstowin(hand, 1))
+    // Try holding one card (note array is reversed at this point)
+    odds = append(odds, oddstowin(hand, 1))
+    hand[0], hand[2] = hand[2], hand[0]
+    odds = append(odds, oddstowin(hand, 1))
+    hand[1], hand[0] = hand[0], hand[1]
+    odds = append(odds, oddstowin(hand, 1))
 
-  // Try discarding all
-  odds = append(odds, oddstowin(hand, 0))
+    // Try discarding all
+    odds = append(odds, oddstowin(hand, 0))
 
-  // Which one is highest?
-  for index, odd := range odds {
-    if (odd > bestodds) {
-      bestodds = odd
-      bestplay = bestMapping[index]
+    // Which one is highest?
+    for index, odd := range odds {
+        if (odd > bestodds) {
+            bestodds = odd
+            bestplay = bestMapping[index]
+        }
     }
-  }
-  return bestplay, bestodds
+    ch <- bestplay
 }
 
 func handtostring(hand []string) string {
@@ -132,54 +133,83 @@ func handtostring(hand []string) string {
 }
 
 func main() {
-  // Load the JSON files in
-  dat := Ranks()
-  err := json.Unmarshal(dat, &ranking)
-  if err != nil {
-    fmt.Println(err.Error())
-    return
-  }
-
-  dat = Winners()
-  err = json.Unmarshal(dat, &winners)
-  if err != nil {
-    fmt.Println(err.Error())
-    return
-  }
-
-  // Set up the deck
-  var card string
-  suits := []string{"C", "D", "H", "S"}
-  for i := 0; i < 13; i++ {
-    for j := 0; j < 4; j++ {
-      if (i < 9) {
-        card = strconv.Itoa(i + 2)
-      } else if (i == 9) {
-        card = "J"
-      } else if (i == 10) {
-        card = "Q"
-      } else if (i == 11) {
-        card = "K"
-      } else {
-        card = "A"
-      }
-
-      card += suits[j]
-      deck = append(deck, card)
+    // Load the JSON files in
+    dat := Ranks()
+    err := json.Unmarshal(dat, &ranking)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
     }
-  }
 
-  // And start analyzing hands
-  var suggestions = make(map[string][]int)
-  start := time.Now()
-  for hand := range ranking {
-    bestplay, _ := analyzehand(hand)
-    suggestions[hand] = bestplay
-  }
-  t := time.Now()
-  elapsed := t.Sub(start)
+    dat = Winners()
+    err = json.Unmarshal(dat, &winners)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
 
-  result, _ := json.Marshal(suggestions)
-  fmt.Println(string(result))
-  fmt.Println(elapsed)
+    // Set up the deck
+    var card string
+    suits := []string{"C", "D", "H", "S"}
+    for i := 0; i < 13; i++ {
+        for j := 0; j < 4; j++ {
+            if (i < 9) {
+                card = strconv.Itoa(i + 2)
+            } else if (i == 9) {
+                card = "J"
+            } else if (i == 10) {
+                card = "Q"
+            } else if (i == 11) {
+                card = "K"
+            } else {
+                card = "A"
+            }
+
+            card += suits[j]
+            deck = append(deck, card)
+        }
+    }
+
+    // And start analyzing hands
+    // Parallelize over mutlple channels
+    const NumberOfChannels = 4
+    var i int
+    var bestplay []int
+    var suggestions = make(map[string][]int)
+    start := time.Now()
+    channels := make([]chan []int, NumberOfChannels)
+    for i = 0; i < len(channels); i++ {
+        channels[i] = make(chan []int)
+    }
+
+    keys := make([]string, len(ranking))
+    i = 0
+    for k := range ranking {
+        keys[i] = k
+        i++
+    }
+
+    for i = 0; i < len(keys) - (NumberOfChannels - 1); i += NumberOfChannels {
+        for j, ch := range channels {
+            go analyzehand(ch, keys[i + j])
+        }
+        for j, ch := range channels {
+            bestplay = <- ch
+            suggestions[keys[i + j]] = bestplay
+        }
+    }
+
+    // May need to do one more if there were odd number of keys
+    for j := 0; j < len(keys) % NumberOfChannels; j++ {
+        go analyzehand(channels[0], keys[len(keys) - j - 1])
+        bestplay = <- channels[0]
+        suggestions[keys[len(keys) - j - 1]] = bestplay
+    }
+
+    t := time.Now()
+    elapsed := t.Sub(start)
+
+    result, _ := json.Marshal(suggestions)
+    ioutil.WriteFile("suggest.json", result, 0644)
+    fmt.Println(elapsed)
 }
